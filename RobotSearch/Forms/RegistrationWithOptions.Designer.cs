ab828e99ed4d11f578f117a9c0b773164e03e890
J-Veber <kovalsky.corp@gmail.com>
2017-04-25 11:37:37 +1000

    bug fixed

---
+++ RobotSearch/Forms/RegistrationWithOptions.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace RobotSearch.Forms
+{
+    partial class RegistrationWithOptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistrationWithOptions));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.stripbtn_Exit = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox_regAdmin = new System.Windows.Forms.CheckBox();
+            this.button_regAdm = new System.Windows.Forms.Button();
+            this.tb_UserPassw_regAdm = new System.Windows.Forms.TextBox();
+            this.tb_UserName_regAdm = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stripbtn_Exit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(432, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // stripbtn_Exit
+            // 
+            this.stripbtn_Exit.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.stripbtn_Exit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.stripbtn_Exit.Image = ((System.Drawing.Image)(resources.GetObject("stripbtn_Exit.Image")));
+            this.stripbtn_Exit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.stripbtn_Exit.Name = "stripbtn_Exit";
+            this.stripbtn_Exit.Size = new System.Drawing.Size(45, 22);
+            this.stripbtn_Exit.Text = "Выход";
+            this.stripbtn_Exit.Click += new System.EventHandler(this.stripbtn_Exit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox_regAdmin);
+            this.groupBox1.Controls.Add(this.button_regAdm);
+            this.groupBox1.Controls.Add(this.tb_UserPassw_regAdm);
+            this.groupBox1.Controls.Add(this.tb_UserName_regAdm);
+            this.groupBox1.Location = new System.Drawing.Point(33, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(365, 170);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Введите данные для регистрации";
+            // 
+            // checkBox_regAdmin
+            // 
+            this.checkBox_regAdmin.AutoSize = true;
+            this.checkBox_regAdmin.Location = new System.Drawing.Point(16, 124);
+            this.checkBox_regAdmin.Name = "checkBox_regAdmin";
+            this.checkBox_regAdmin.Size = new System.Drawing.Size(108, 17);
+            this.checkBox_regAdmin.TabIndex = 4;
+            this.checkBox_regAdmin.Text = "Администратор ";
+            this.checkBox_regAdmin.UseVisualStyleBackColor = true;
+            // 
+            // button_regAdm
+            // 
+            this.button_regAdm.Location = new System.Drawing.Point(265, 65);
+            this.button_regAdm.Name = "button_regAdm";
+            this.button_regAdm.Size = new System.Drawing.Size(85, 62);
+            this.button_regAdm.TabIndex = 2;
+            this.button_regAdm.Text = "Регистрация";
+            this.button_regAdm.UseVisualStyleBackColor = true;
+            this.button_regAdm.Click += new System.EventHandler(this.button_regAdm_Click);
+            // 
+            // tb_UserPassw_regAdm
+            // 
+            this.tb_UserPassw_regAdm.Location = new System.Drawing.Point(16, 87);
+            this.tb_UserPassw_regAdm.Name = "tb_UserPassw_regAdm";
+            this.tb_UserPassw_regAdm.Size = new System.Drawing.Size(215, 20);
+            this.tb_UserPassw_regAdm.TabIndex = 1;
+            this.tb_UserPassw_regAdm.Text = "Введите пароль";
+            // 
+            // tb_UserName_regAdm
+            // 
+            this.tb_UserName_regAdm.Location = new System.Drawing.Point(16, 45);
+            this.tb_UserName_regAdm.Name = "tb_UserName_regAdm";
+            this.tb_UserName_regAdm.Size = new System.Drawing.Size(215, 20);
+            this.tb_UserName_regAdm.TabIndex = 0;
+            this.tb_UserName_regAdm.Text = "Введите логин";
+            // 
+            // RegistrationWithOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 333);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "RegistrationWithOptions";
+            this.Text = "RegistrationWithAdmin";
+            this.Load += new System.EventHandler(this.RegistrationWithOptions_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton stripbtn_Exit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_regAdm;
+        private System.Windows.Forms.TextBox tb_UserPassw_regAdm;
+        private System.Windows.Forms.TextBox tb_UserName_regAdm;
+        private System.Windows.Forms.CheckBox checkBox_regAdmin;
+    }
+}
---
+++ RobotSearch/Forms/RegistrationWithOptions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RobotSearch.Forms
+{
+    public partial class RegistrationWithOptions : Form
+    {
+        public Form MyForm;
+        private string _userLogin;
+        private string _userPassw;
+        private int _userRights; // 1 is Admin
+        private int _userID;
+
+        public RegistrationWithOptions(Form inputForm)
+        {
+            InitializeComponent();
+            MyForm = inputForm;
+            tb_UserName_regAdm.Clear();
+            tb_UserPassw_regAdm.Clear();
+        }
+
+
+
+        private void stripbtn_Exit_Click(object sender, EventArgs e)
+        {
+            MyForm.Enabled = true;
+            this.Close();
+        }
+
+        private void button_regAdm_Click(object sender, EventArgs e)
+        {
+            _userLogin = Convert.ToString(tb_UserName_regAdm.Text);
+            _userPassw = Convert.ToString(tb_UserPassw_regAdm.Text);
+            if (checkBox_regAdmin.Checked) _userRights = 1;
+            else _userRights = 0;
+
+            SqlConnection connection = new SqlConnection
+            {
+                ConnectionString =
+                    (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\koval\documents\visual studio 2017\Projects\RobotSearch\RobotSearch\robot_search_db.mdf;Integrated Security=True")
+            };
+
+
+            using (SqlCommand command = new SqlCommand("SELECT * FROM users " +
+                                                       "WHERE users_login='" + _userLogin + "' AND users_passw='" + _userPassw + "';", connection))
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    MessageBox.Show("Пользователь с таким логином или паролем уже существует.");
+                    tb_UserName_regAdm.Clear();
+                    tb_UserPassw_regAdm.Clear();
+                    checkBox_regAdmin.Checked = false;
+                    reader.Close();
+                    connection.Close();
+                }
+                else
+                {
+                    reader.Close();
+                    string sqlcommand = "INSERT INTO dbo.users (users_login, users_passw, users_admin) VALUES (@userLogin, " +
+                                        "@userPassw, @userRights);";
+                    using (var sqlSaveCommand = new SqlCommand(sqlcommand, connection))
+                    {
+                        sqlSaveCommand.Parameters.Add("@userLogin", SqlDbType.VarChar, 15).Value = _userLogin;
+                        sqlSaveCommand.Parameters.Add("@userPassw", SqlDbType.VarChar, 10).Value = _userPassw;
+                        sqlSaveCommand.Parameters.Add("@userRights", SqlDbType.TinyInt).Value = _userRights;
+
+                        sqlSaveCommand.ExecuteNonQuery();
+
+                        if (_userRights == 1)
+                        {
+                            MessageBox.Show("Пользователь " + _userLogin + " успешно добавлен с паролем " + _userPassw
+                                            + " и с правами администратора.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Пользователь " + _userLogin + " успешно добавлен с паролем " + _userPassw
+                                            + " и с правами пользователя.");
+                        }
+                    }
+                }
+                connection.Close();
+            }
+        }
+
+        private void RegistrationWithOptions_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
